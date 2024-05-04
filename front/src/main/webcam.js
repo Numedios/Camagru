d07981f393dc1d8ee6a1bb167bbfe986714c782a
sofiane belabbas <sofianebelabbas93220@gmail.com>
2024-05-04 14:21:32 +0200

    feat : add button can activate or deactivate the webcam by a click

--- front/src/main/webcam.js
+++ front/src/main/webcam.js
@@ -1,40 +1,60 @@
 
 
-var video = document.getElementById('sourcevid');
-var takePicture = document.getElementById('takepicture');
-var stockPicture = document.getElementById('stockpicture');
-var activeWebcam = document.getElementById('activate-webcam');
+const video = document.getElementById('sourcevid');
+const takePicture = document.getElementById('takepicture');
+const stockPicture = document.getElementById('stockpicture');
+const  activeWebcam = document.getElementById('activate-webcam');
+
+var boolWebcam = false;
+var tracks;
 
 function open_webcam() {
-    navigator.mediaDevices.getUserMedia({audio: false, video: {width: 350, height: 350}})
-        .then(function(mediaStream) {
-            activeWebcam.textContent = 'Desactivate Webcam'
-            var video = document.getElementById('sourcevid');
-            video.srcObject = mediaStream;
+    if (!boolWebcam)
+    {
+        navigator.mediaDevices.getUserMedia({audio: false, video: {width: 350, height: 350}})
+            .then(function(mediaStream) {
+                var video = document.getElementById('sourcevid');
+                video.srcObject = mediaStream;
 
-            var tracks = mediaStream.getTracks();
-            console.log(tracks)
-            video.onloadedmetadata  = function(e) {
-                video.play();
-            };
-            
-        })
-        .catch(function(err) { 
-            console.log(err.name + ": " + err.message); 
-            ////document.getElementById("message").innerHTML="message: connection refusé" 
+                tracks = mediaStream.getTracks();
+                console.log(tracks)
+                video.onloadedmetadata  = function(e) {
+                    video.play();
+                };
+                boolWebcam = true;
+                activeWebcam.textContent = 'Desactivate Webcam';
+
+            })
+            .catch(function(err) { 
+                // chercher les type d'erreur possible
+                console.log(err.name + ": " + err.message); 
+                //document.getElementById("message").innerHTML="message: connection refusé" 
+            });
+    }
+    else
+    {
+        tracks.forEach(function(track) {
+            track.stop();
         });
+        boolWebcam = false;
+        activeWebcam.textContent = 'Activate Webcam';
+        console.log("cam desactivate");
+    }
 }
 
 function photo(){
-    var vivi = document.getElementById('sourcevid');
-    var canvas = document.createElement("canvas");
-    canvas.classList.add("pictures");
-    var ctx = canvas.getContext('2d');
-    canvas.height=vivi.videoHeight
-    canvas.width=vivi.videoWidth
-    ctx.drawImage(vivi, 0,0, vivi.videoWidth, vivi.videoHeight);
-    stockPicture.appendChild(canvas);
+    if (boolWebcam)
+    {
+        var vivi = document.getElementById('sourcevid');
+        var canvas = document.createElement("canvas");
+        canvas.classList.add("pictures");
+        var ctx = canvas.getContext('2d');
+        canvas.height=vivi.videoHeight
+        canvas.width=vivi.videoWidth
+        ctx.drawImage(vivi, 0,0, vivi.videoWidth, vivi.videoHeight);
+        stockPicture.appendChild(canvas);
+    }
 }
 
-// takePicture.addEventListener("click", photo);
+takePicture.addEventListener("click", photo);
 activeWebcam.addEventListener("click", open_webcam);
